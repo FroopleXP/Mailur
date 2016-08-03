c3b15ef98ec545235c77e78d7d67f7d7fe16ed85
Connor Edwards <connor@frooplexp.com>
2016-08-03 16:28:03 +0100

    Done somethings...

--- mailur.js
+++ mailur.js
@@ -1,19 +1,44 @@
 // Requiring dependencies
 var express = require("express"),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    xss_filter = require('xss-filters'),
     app = express();
 
 // Including config files
-var server_config = require("config/app_config.js");
+var server_config = require("./config/app_config.js"),
+    api = express.Router();
 
-// Setting rendering engine
+// Configuring the App
 app.set('view engine', 'ejs');
+app.use("/views", express.static(__dirname + '/views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api', api);
+app.use(cors());
 
-// Routes
+// Site routes
 app.get("/", function(req, res) {
-    res.render('home', { title: "Mailur - Home" });
+    res.render('home', { title: "Mailur" });
+});
+
+// API routes
+api.post("/register", function(req, res) {
+
+    // Getting and cleaning the data from the form
+    var username = xss_filter.inHTMLData(req.body.username),
+        email = xss_filter.inHTMLData(req.body.email),
+        password = xss_filter.inHTMLData(req.body.password),
+        confirm_password = xss_filter.inHTMLData(req.body.confirm_password);
+
+    // Validating the data
+    console.log(username);
+
+
+
 });
 
 // Starting the app
-app.listen(server_config, funciton() {
+app.listen(server_config.app.port, function() {
     console.log("Mailur server started on port: " + server_config.app.port);
 });
---
+++ views/app/mailur_app.js
@@ -0,0 +1,71 @@
+// Declaring the app
+angular.module("mailur", ["ngRoute"])
+
+// Configuring the Routes
+.config(function($routeProvider) {
+
+    $routeProvider
+    .when("/", {
+        templateUrl: "views/app_views/home.htm",
+        controller: 'home_ctrl'
+    })
+    .when("/dashboard", {
+        templateUrl: "views/app_views/dashboard.htm",
+        controller: 'dashboard_ctrl'
+    })
+    .when("/register", {
+        templateUrl: "views/app_views/register.htm",
+        controller: 'register_ctrl'
+    })
+
+})
+
+// Controllers
+.controller('home_ctrl', function($scope) {
+
+})
+
+.controller('dashboard_ctrl', function($scope) {
+
+})
+
+.controller('register_ctrl', function($scope, regsiter_serv) {
+
+    // Function used to register the user
+    $scope.register = function(register_details) {
+        regsiter_serv.register(register_details)
+        .then(function(success) {
+            alert(success);
+        })
+        .catch(function(err) {
+            alert(err);
+        })
+    }
+
+})
+
+// Factorys
+.factory('regsiter_serv', function($q, $http) {
+    return {
+        register: function(register_details) {
+            // Sending the details to the API, creating the promise
+            var q = $q.defer();
+            $http.post("http://localhost:1337/api/register", register_details)
+            .then(function(response) { // We have a response with code 200
+                q.resolve("You've successfully registered!");
+            })
+            .catch(function(err) { // There was an error, something other than status 200
+                q.reject(err.message);
+            })
+            return q.promise;
+        }
+    }
+})
+
+.factory('redirect', function($state) {
+    return {
+        go: function(where_to) {
+            $state.go(where_to);
+        }
+    }
+})
